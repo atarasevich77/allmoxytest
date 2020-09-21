28616d0ea2752f3597f66f7591e12854cb95dfd9
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-09-21 17:23:39 -0600

    Add mongodb connect

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 mongoose.set('useCreateIndex', true);
 mongoose.connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
-    // { useNewUrlParser: true },
+    { useNewUrlParser: true },
 );
 // mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/allmoxytest`);
 
