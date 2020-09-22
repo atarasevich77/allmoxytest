808ec507a30a94924a2c66e27c950688af71d0c5
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-09-21 22:57:30 -0600

    Add mongodb connect

--- client/src/App.js
+++ client/src/App.js
@@ -5,36 +5,72 @@ function App() {
     const [products, setProducts] = useState(null);
 
     useEffect(() => {
-        if (!products) {
-            productService.getAll()
-                .then(response => {
-                    console.log(response);
-                    setProducts(response);
-                })
-                .catch(error =>
-                    console.log(error)
-                )
-        }
-    });
+        productService.getAll()
+            .then(response => {
+                setProducts(response);
+            })
+            .catch(error =>
+                console.log(error)
+            )
+    }, []);
 
     return (
-        <div className="App">
-            <ul className="list">
-                {(products && products.length > 0)
-                    ? (
-                            products.map(product =>
-                                (
-                                    <li key={product._id} className="list_item product">
-                                        <h3 className="product_title">{product.title}</h3>
-                                        <p className="product_description">{product.description}</p>
-                                    </li>
-                                )
-                            )
+        <div className="container-fluid">
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    (products && products.length > 0) ?
+                        products.map((product, index) =>
+                            <tr key={product._id}>
+                                <th>
+                                    {++index}
+                                </th>
+                                <td>
+                                    {product.title}
+                                </td>
+                                <td>
+                                    {product.description}
+                                </td>
+                                <td>
+                                    {product.price}
+                                </td>
+                                <td>
+                                    {product.quantity}
+                                </td>
+                                <td>
+                                    {product.image}
+                                </td>
+                                <td>
+                                    <button type="button" className="btn btn-primary">Edit</button>
+                                </td>
+                                <td>
+                                    <button type="button" className="btn btn-primary">Delete</button>
+                                </td>
+                            </tr>
                         )
-                    :
-                    <p>No products found</p>
+                        :
+                        <tr>
+                            <td colSpan="8" className="d-flex justify-content-center">
+                                <div className="spinner-border" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                            </td>
+                        </tr>
                 }
-            </ul>
+                </tbody>
+            </table>
         </div>
     );
 }
